49e9540839c020eace3a78542ccd0ed661127f91
LibrechtKuijvenhoven <librecht@live.nl>
2025-03-24 12:06:02 +0100

    initial commit, with postgresql

---
+++ postgresql/generate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+alias antlr4='java -Xmx500M -cp "../antlr-4.13.0-complete.jar" org.antlr.v4.Tool'
+echo "[info] Tranforming grammar for go"
+python3 ./transformGrammar.py
+echo "[info] Generating Go file from grammar files"
+antlr4 -Dlanguage=Go -no-visitor -o parser *.g4
